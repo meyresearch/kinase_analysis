29ff95a436fc9ef4473ee22484fa65152d8d1188
rzhu <rzhu@neptune.chem.ed.ac.uk>
2024-02-29 17:15:36 +0000

    Add featurisation functions for egfr variable motifs

--- funcs_featurise.py
+++ funcs_featurise.py
@@ -3,57 +3,158 @@ import numpy as np
 from typing import *
 
 
-def dunbrack_egfr_featurizer(traj) -> np.ndarray:
-    # The dunbrack featurizer for the EGFR kinase domain
+def find_atomid(top, atom_name) -> int:
+    atomid = np.where([str(atom) == atom_name for atom in top.atoms])[0]
+    assert len(atomid) == 1
 
-    ########## Spatial groups ############
+    return atomid[0]
+
+
+def dbdist_featuriser(traj, save_to_disk=None) -> np.ndarray:
+    ########## Dunbrack DFG distances ############
     # d1 = dist(αC-Glu(+4)-Cα, DFG-Phe-Cζ) 
     # Distance between the Ca of the fourth residue after the conserved Glu (Met) in the C-helix
     # and the outermost atom of the DFG-Phe ring Cζ
     # d2 = dist(β3-Lys-Cα, DFG-Phe-Cζ) 
-    ######################################
-    
-    met_ca_id = 1026
-    phe_cz_id = 2496
-    lys_ca_id = 687
-    
+    ##############################################
     top = traj.topology
-    assert str(top.atom(met_ca_id)) == 'MET766-CA'
-    assert str(top.atom(phe_cz_id)) == 'PHE856-CZ'
-    assert str(top.atom(lys_ca_id)) == 'LYS745-CA'
-    d = md.compute_distances(traj, [[met_ca_id, phe_cz_id], [lys_ca_id, phe_cz_id]])
-    
-    ########## Dihedral groups ############
+
+    met_ca_id = find_atomid(top, 'MET766-CA')
+    phe_cz_id = find_atomid(top, 'PHE856-CZ')
+    lys_ca_id = find_atomid(top, 'LYS745-CA')
+
+    distances = md.compute_distances(traj, [[met_ca_id, phe_cz_id], [lys_ca_id, phe_cz_id]])
+    if save_to_disk is not None: np.save(save_to_disk, distances)
+    return distances
+
+
+def dbdihed_featuriser(traj, save_to_disk=None) -> np.ndarray:
+    ########## Dunbrack DFG dihedrals ############
     # The backbone dihedrals of 
     # X-DFG (residue before conserved Asp, Thr), DFG-Asp, DFG-Phe;
     # and the side chain dihedral (χ1) of DFG-Phe
-    ######################################
+    ##############################################
+    top = traj.topology
+
+    xx_dfg_c_id = find_atomid(top, 'ILE853-C')
+    x_dfg_n_id = find_atomid(top, 'THR854-N')
+    x_dfg_ca_id = find_atomid(top, 'THR854-CA')
+    x_dfg_c_id = find_atomid(top, 'THR854-C')
+    dfg_d_n_id = find_atomid(top, 'ASP855-N')
+    dfg_d_ca_id = find_atomid(top, 'ASP855-CA')
+    dfg_d_c_id = find_atomid(top, 'ASP855-C')
+    dfg_f_n_id = find_atomid(top, 'PHE856-N')
+    dfg_f_ca_id = find_atomid(top, 'PHE856-CA')
+    dfg_f_c_id = find_atomid(top, 'PHE856-C')
+    dfg_g_n_id = find_atomid(top, 'GLY857-N')
+    dfg_g_ca_id = find_atomid(top, 'GLY857-CA')
+    dfg_g_c_id = find_atomid(top, 'GLY857-C')
+    dfg_x_n_id = find_atomid(top, 'LEU858-N')
+    dfg_f_cb_id = find_atomid(top, 'PHE856-CB')
+    dfg_f_cg_id = find_atomid(top, 'PHE856-CG')
+
+    dihedrals = md.compute_dihedrals(traj, 
+                                     [[xx_dfg_c_id, x_dfg_n_id, x_dfg_ca_id, x_dfg_c_id], # X-DFG phi
+                                      [x_dfg_n_id, x_dfg_ca_id, x_dfg_c_id, dfg_d_n_id],  # X-DFG psi
+                                      [x_dfg_c_id, dfg_d_n_id, dfg_d_ca_id, dfg_d_c_id],  # DFG-D phi
+                                      [dfg_d_n_id, dfg_d_ca_id, dfg_d_c_id, dfg_f_n_id],  # DFG-D psi
+                                      [dfg_d_c_id, dfg_f_n_id, dfg_f_ca_id, dfg_f_c_id],  # DFG-F phi
+                                      [dfg_f_n_id, dfg_f_ca_id, dfg_f_c_id, dfg_g_n_id],  # DFG-F psi
+                                      [dfg_f_c_id, dfg_g_n_id, dfg_g_ca_id, dfg_g_c_id],  # DFG-G phi
+                                      [dfg_g_n_id, dfg_g_ca_id, dfg_g_c_id, dfg_x_n_id],  # DFG-G psi
+                                      [dfg_f_n_id, dfg_f_ca_id, dfg_f_cb_id, dfg_f_cg_id]]) # DFG-F chi1
     
+    '''
     idx_psi, psi = md.compute_psi(traj)
     idx_phi, phi = md.compute_phi(traj)
     idx_chi, chi = md.compute_chi1(traj)
-
     phi_f = phi[:, 150:153]
     psi_f = psi[:, 150:153]
     phe_chi1 = chi[:, 134]
-    
     assert (idx_phi[150] == [2443, 2460, 2461, 2462]).all()
     assert (idx_psi[150] == [2460, 2461, 2462, 2474]).all()
     assert (idx_chi[134] == [2486, 2487, 2490, 2491]).all()
-    
+
     # Convert angles to sin and cos
     cos = np.cos(np.concatenate([phi_f, psi_f, phe_chi1[:, np.newaxis]], axis=1))
     sin = np.sin(np.concatenate([phi_f, psi_f, phe_chi1[:, np.newaxis]], axis=1))
+    '''
 
-    return np.concatenate([d, cos, sin], axis=1)
+    if save_to_disk is not None: np.save(save_to_disk, dihedrals)
+    return dihedrals
 
 
-def featurize(featurizers: List[function], traj: md.Trajectory) -> np.ndarray:
+def aloop_featuriser(traj, save_to_disk=None) -> np.ndarray:
+    ########## Activation loop contacts ############
+    # Pairwise distances between Cα atoms of residue i and i+3
+    # Activation loop starts from ASP855 and ends at PRO877
+    ################################################
+    top = traj.topology
+
+    asp855_id = np.where([str(res) == 'ASP855' for res in top.residues])[0][0]
+    pro877_id = np.where([str(res) == 'PRO877' for res in top.residues])[0][0]
+    aloop_traj = traj.atom_slice(top.select(f'resid {asp855_id} to {pro877_id}'))
+
+    distances = md.compute_contacts(aloop_traj, contacts='all', scheme='ca')[0]
+    if save_to_disk is not None: np.save(save_to_disk, distances)
+    return distances
+
+
+def ploop_featuriser(traj, save_to_disk=None) -> np.ndarray:
+    ########## phosphate-binding loop distances ############
+    # d1 = dist(Cα-Leu718, Cα-Phe723) * maybe use Gly724 because it's conserved
+    # d2 = dist(Cα-Gly719, Cα-Ala722)
+    ########################################################
+    top = traj.topology
+
+    leu718_ca_id = find_atomid(top, 'LEU718-CA')
+    phe723_ca_id = find_atomid(top, 'PHE723-CA')
+    gly719_ca_id = find_atomid(top, 'GLY719-CA')
+    ala722_ca_id = find_atomid(top, 'ALA722-CA')
+
+    distances = md.compute_distances(traj, [[leu718_ca_id, phe723_ca_id], [gly719_ca_id, ala722_ca_id]])
+    if save_to_disk is not None: np.save(save_to_disk, distances)
+    return distances
+
+
+def achelix_featuriser(traj, save_to_disk=None) -> np.ndarray:
+    ########## phosphate-binding loop distances ############
+    # d1 = dist(Nz-Lys745, Cd-Glu762) 
+    # d2 = dist(Ce-Lys860, Cd-Glu762) 
+    ########################################################
+    top = traj.topology
+
+    lys745_nz_id = find_atomid(top, 'LYS745-NZ')
+    glu762_cd_id = find_atomid(top, 'GLU762-CD')
+    lys860_ce_id = find_atomid(top, 'LYS860-CE')
+
+    distances = md.compute_distances(traj, [[lys745_nz_id, glu762_cd_id], [lys860_ce_id, glu762_cd_id]])
+    if save_to_disk is not None: np.save(save_to_disk, distances)
+    return distances
+
+
+def rspine_featuriser(traj, save_to_disk=None) -> np.ndarray:
+    ########## phosphate-binding loop distances ############
+    # d1 = dist(Cg-Leu777, Sd-M766) 
+    # d2 = dist(Cg-Phe856, Sd-M766) 
+    ########################################################
+    top = traj.topology
+
+    leu777_cg_id = find_atomid(top, 'LEU777-CG')
+    met766_sd_id = find_atomid(top, 'MET766-SD')
+    phe856_cg_id = find_atomid(top, 'PHE856-CG')
+
+    distances = md.compute_distances(traj, [[leu777_cg_id, met766_sd_id], [phe856_cg_id, met766_sd_id]])
+    if save_to_disk is not None: np.save(save_to_disk, distances)
+    return distances
+
+
+def featurise(featurisers: List, traj: md.Trajectory) -> np.ndarray:
     assert type(traj) == md.Trajectory
     
     ftraj_list = []
-    for featurizer in featurizers:
-        ftraj = featurizer(traj)
+    for featuriser in featurisers:
+        ftraj = featuriser(traj)
         if ftraj.dim == 1: 
             ftraj = ftraj[:, np.newaxis]
         ftraj_list.append(ftraj)
--- run_featurise_egfr.py
+++ run_featurise_egfr.py
@@ -1,42 +1,58 @@
 import re
+import os
 from natsort import natsorted
 from tqdm import tqdm
 from pathlib import Path
-
+import builtins
 from funcs_featurise import *
+
+original_print = builtins.print
+def print(*args, **kwargs):
+    kwargs.setdefault('flush', True)  # Set flush to True unless it's already specified
+    original_print(*args, **kwargs)
+builtins.print = print
 
 
 if __name__ == "__main__":
-    traj_dir = Path('/arc/EGFR/')
+    traj_dir = Path('/arc/egfr_processed')
     traj_files = natsorted([traj for traj in traj_dir.rglob('run*-clone?.h5')])
     max_run_no = max([int(re.search(r'run([0-9]+)-clone[0-9]+\.h5', f.name).group(1)) for f in traj_files])
 
     save_dir = Path('/home/rzhu/Desktop/projects/kinase_analysis/data_egfr/ftrajs/')
     if not save_dir.exists(): save_dir.mkdir(parents=True, exist_ok=True)
 
-    ref = md.load('/home/rzhu/Desktop/projects/kinase_analysis/human_egfr/kinoml_OEKLIFSKinaseApoFeaturizer_EGFR_1m14_chainA_altlocNone_protein.pdb')
+    ref = md.load('/arc/egfr_equilibrated_strucs/RUN0_solute_equilibrated.pdb')
     backbone_atomids = ref.topology.select('backbone')
+    
+    featurisers = [dbdist_featuriser, dbdihed_featuriser, aloop_featuriser, ploop_featuriser, achelix_featuriser, rspine_featuriser]
+    print(f'Featurisers: {[f.__name__ for f in featurisers]}')
 
-    # Loop over each starting conformation 
-    for i in tqdm(range(max_run_no), total=max_run_no):
-        trajs = [p for p in traj_dir.rglob(f'run{i}-clone?.h5')]
-        
-        # Loop over each repetition 
-        for traj in trajs:
-            if save_dir.joinpath(f'{traj.stem}_dunbrack.npy').is_file():
-                print(traj, 'Already exist.')
-                continue
-            try:
-                md_traj = md.load(traj)
-            except:
-                with open(save_dir.joinpath('log.txt'), 'a') as f:
-                    f.writelines(f'Fail to read {traj} \n')
-                print(f'Fail to read {traj} \n')
-                continue
-                
-            print(traj, len(md_traj))
-            md_traj = md_traj.superpose(ref, atom_indices=backbone_atomids)
+    # Loop over runs
+    try:
+        for i in tqdm(range(max_run_no), total=max_run_no):
+            trajs = natsorted([p for p in traj_dir.rglob(f'run{i}-clone?.h5')])
+            print(f'Run {i}: {len(trajs)} trajectories to be featurised.')
 
-            ftraj = featurize([dunbrack_egfr_featurizer], md_traj)
-            with open(save_dir.joinpath(f'{traj.stem}_dunbrack.npy'),'wb') as f:
-                np.save(f, ftraj)
+            # Loop over clones
+            for traj in trajs:
+                print('Featurising', traj.stem)
+                try:
+                    md_traj = md.load(traj)
+                    md_traj = md_traj.superpose(ref, atom_indices=backbone_atomids)
+                except:
+                    print(f'!!! Fail to read {traj} !!!')
+                    continue
+
+                for featuriser in featurisers:
+                    ftraj_dir = save_dir.joinpath(f"{traj.stem}_{featuriser.__name__.split('_')[0]}.npy")
+                    if ftraj_dir.is_file():
+                        print(traj.stem, f"{featuriser.__name__.split('_')[0]} ftraj already exist.")
+                        continue
+                    try:
+                        _ = featuriser(md_traj, save_to_disk=ftraj_dir)
+                        print('Featurised', traj.stem, f"{featuriser.__name__.split('_')[0]} ftraj.")
+                    except Exception as err:
+                        if os.path.exists(ftraj_dir): os.remove(ftraj_dir)
+                        print(f"Fail to featurise {traj.stem} with {featuriser.__name__.split('_')[0]}:\n{err}")
+    except KeyboardInterrupt:
+        print('Keyboard interrupt. Exiting.')
