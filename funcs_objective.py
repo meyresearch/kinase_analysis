514d6d64be315a3200332e08f10424e2aa7711c3
rzhu <rzhu@neptune.chem.ed.ac.uk>
2024-03-14 15:32:47 +0000

    Reduce precision for more features

--- funcs_objective.py
+++ funcs_objective.py
@@ -16,18 +16,16 @@ import pandas as pd
 from pathlib import Path
 
 
-def objective(trial, study_name, trial_key, markov_lag, features, ftraj_dir) -> Tuple[float, float, float, float]:
+def objective(trial, study_name, trial_key, markov_lag, ftrajs_all, cutoff) -> Tuple[float, float, float, float]:
     start_time = time.time()
 
     n_boot = 20
     seed = 49587
     score_k = 2
     fitting_func = estimate_msm
-
     rng = np.random.default_rng(seed)
 
-    hp_dict = define_hpdict(trial, study_name, trial_key)
-    ftrajs_all, _ = get_data(hp_dict, features, ftraj_dir)
+    hp_dict = define_hpdict(trial, study_name, trial_key, cutoff)
 
     ftraj_lens = [x.shape[0] for x in ftrajs_all]
     ixs = [bootstrap(ftraj_lens, rng) for _ in range(n_boot)]
@@ -46,23 +44,26 @@ def objective(trial, study_name, trial_key, markov_lag, features, ftraj_dir) -> 
     return res0, res1, res2, res3
 
 
-def get_data(hp_dict, features, ftraj_dir) -> Tuple[List[np.ndarray], Dict[int, int]]:
+def get_data(trajlen__cutoff, features, ftraj_dir) -> Tuple[List[np.ndarray], Dict[int, int]]:
     # Load selected feature trajectories
     ftrajs_all = []
     for feature in features:
         assert feature in ['dbdist', 'dbdihed', 'aloop', 'ploop', 'achelix', 'rspine'], 'Feature not recognised.'
         ftraj_files = natsorted([str(ftraj) for ftraj in ftraj_dir.rglob(f'run*-clone?_{feature}.npy')])
-        ftrajs = [np.load(ftraj_file, allow_pickle=True) for ftraj_file in ftraj_files]
+        ftrajs = [np.load(ftraj_file, allow_pickle=True).astype(np.float16) for ftraj_file in ftraj_files]
         # Convert dihedral angles to sin and cos pairs
         if 'dihed' in feature: ftrajs = [np.concatenate([np.cos(ftraj), np.sin(ftraj)], axis=1) for ftraj in ftrajs]
         ftrajs_all.append(ftrajs)
+
     print(f'Total number of ftrajs: {len(ftrajs_all[0])}.')
-    ftrajs_cat = [np.concatenate([lst[i] for lst in ftrajs_all], axis=1) for i in range(len(ftrajs))]
+    ftrajs_cat = [np.concatenate([lst[i] for lst in ftrajs_all], axis=1).astype(np.float32) for i in range(len(ftrajs))]
+    del ftrajs_all
+    #ftrajs_cat = [ftraj.astype(np.float32) for ftraj in ftrajs_cat]
 
     # Remove trajectories with length less than the cutoff
-    # Meanwhile reate a mapping from the new indices to the old indices
+    # Meanwhile create a mapping from the new indices to the old indices
     ftrajs_len = np.array([ftraj.shape[0] for ftraj in ftrajs_cat])
-    ftrajs_len_mask = ftrajs_len > hp_dict.trajlen__cutoff
+    ftrajs_len_mask = ftrajs_len > trajlen__cutoff
     ftrajs_to_load = [ftrajs_cat[i] for i in range(len(ftrajs_cat)) if ftrajs_len_mask[i]]
     mapping = {new_idx: old_idx for new_idx, old_idx in enumerate(np.where(ftrajs_len_mask == 1)[0])}
     print(f'Loaded number of ftrajs:  {len(ftrajs_to_load)}')
@@ -79,13 +80,13 @@ def bootstrap(lengths: np.ndarray, rng: np.random.Generator) -> List[np.ndarray]
     return new_ix
 
 
-def define_hpdict(trial, study_name, trial_key) -> Dict: 
+def define_hpdict(trial, study_name, trial_key, cutoff) -> Dict: 
     hp_dict = Adict()
     hp_dict.trial__no = trial.number
-    hp_dict.trajlen__cutoff = 1000
     hp_dict.tica__lag = trial.suggest_int('tica__lag', 1, 100)
     hp_dict.tica__dim = trial.suggest_int('tica__dim', 2, 20)
     hp_dict.tica__stride = 10
+    hp_dict.trajlen__cutoff = cutoff
     hp_dict.cluster__k = trial.suggest_int('cluster__k', 500, 1500)
     hp_dict.cluster__maxiter = 1000
     hp_dict.cluster__stride = 100
@@ -94,7 +95,7 @@ def define_hpdict(trial, study_name, trial_key) -> Dict:
     assert hp_dict.trajlen__cutoff >= hp_dict.cluster__stride
 
     hp_df = pd.DataFrame(hp_dict, index=[trial.number])
-    hp_df.to_hdf(f'data_egfr/msm/{study_name}.h5', key=f'{trial_key}_hps', mode='a', format='table', append=True, data_columns=True)
+    hp_df.to_hdf(f'data_egfr/msm/allfeature_studies/{study_name}.h5', key=f'{trial_key}_hps', mode='a', format='table', append=True, data_columns=True)
 
     return hp_dict
 
--- run_msm_study.py
+++ run_msm_study.py
@@ -6,21 +6,31 @@ import optuna
 from pathlib import Path
 from functools import partial
 from funcs_objective import *
+import builtins
 
-storage_name = "sqlite:///data_egfr/msm/dunbrack.db"
+'''
+original_print = builtins.print
+def print(*args, **kwargs):
+    kwargs.setdefault('flush', True)  # Set flush to True unless it's already specified
+    original_print(*args, **kwargs)
+builtins.print = print
+'''
 
-study_name = "lag100"
+storage_name = "sqlite:///data_egfr/msm/allfeature_studies/allfeature.db"
+study_name = "lag500"
 trial_key = 'random_trials'
-markov_lag= 100
-features = ['dbdist', 'dbdihed']
+markov_lag= 500
+trajlen__cutoff = 1000
+features = ['dbdist', 'dbdihed', 'ploop', 'aloop', 'achelix', 'rspine']
 ftraj_dir = Path('data_egfr/ftrajs')
 
+ftrajs_all, _ = get_data(trajlen__cutoff, features, ftraj_dir)
 study = optuna.load_study(study_name=study_name, storage=storage_name)
 print(len(study.trials))
 
-objective = partial(objective, study_name=study_name, trial_key=trial_key, markov_lag=markov_lag, features=features, ftraj_dir=ftraj_dir)
-study.optimize(objective, n_trials=18, catch=(ValueError,))
+objective = partial(objective, study_name=study_name, trial_key=trial_key, markov_lag=markov_lag, ftrajs_all=ftrajs_all, cutoff=trajlen__cutoff)
+study.optimize(objective, n_trials=100, catch=(ValueError,))
 print('Sampling finished.')
 print(len(study.trials))
 
-study.trials_dataframe().to_hdf(f'data_egfr/msm/{study_name}.h5', key=trial_key, mode='a')
+study.trials_dataframe().to_hdf(f'data_egfr/msm/allfeature_studies/{study_name}.h5', key=trial_key, mode='a')
