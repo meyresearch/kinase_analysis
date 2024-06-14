2756060dbff81951a3df432093e57c410479fcb0
rzhu <rzhu@neptune.chem.ed.ac.uk>
2024-06-15 00:11:41 +0100

    wrapping up with MSMAnalysis and MSMstudy

---
+++ MSMStudy.py
@@ -0,0 +1,243 @@
+from pyemma.coordinates import tica 
+from pyemma.coordinates import cluster_kmeans
+import pyemma as pm
+
+from deeptime.markov import TransitionCountEstimator
+from deeptime.markov.msm import MaximumLikelihoodMSM
+from deeptime.markov.msm import BayesianMSM
+from deeptime.markov.tools import *
+import deeptime as dt
+
+import numpy as np
+from pathlib import Path
+
+
+class MSMStudy():
+    _allowed_paramters = ['tica__lag', 'tica__stride', 'tica__dim', 
+                          'cluster__k', 'cluster__stride', 'cluster__maxiter', 
+                          'markov__lag', 'pcca_n']
+
+    def __init__(self, study_name, ftrajs, mapping, wk_dir, model_dir=None, **kwargs):
+        self._seed = 42
+        self.study_name = study_name
+        self.ftrajs = ftrajs
+        self.mapping = mapping
+        self.wk_dir = wk_dir
+        if model_dir is None: 
+            self.model_dir = self.wk_dir / 'models'
+        else:
+            if isinstance(model_dir, str): model_dir = Path(model_dir)
+            self.model_dir = model_dir
+
+        for key, value in kwargs.items():
+            if key in self._allowed_paramters:
+                internal_key = f"_{key}"
+                setattr(self, internal_key, value)
+            else:
+                print(f'Parameter {key} not allowed, ignore. Check the allowed parameters')
+                continue
+        
+        if '_tica__lag' not in self.__dict__:
+            self._tica__lag = 1
+            print('TICA lag time not specified. Defaulting to 1')
+        
+        if '_tica__stride' not in self.__dict__:
+            self._tica__stride = 1
+            print('Tica stride not specified. Defaulting to 1')
+        
+        if '_tica__dim' not in self.__dict__:
+            self._tica__dim = 10
+            print('Tica dimension not specified. Defaulting to 2')
+        
+        if '_cluster__k' not in self.__dict__:
+            self._cluster__k = 100
+            print('Number of clusters not specified. Defaulting to 100')
+
+        if '_cluster__stride' not in self.__dict__:
+            self._cluster__stride = 1
+            print('Cluster stride not specified. Defaulting to 1')
+
+        if '_cluster__maxiter' not in self.__dict__: 
+            self._cluster__maxiter = 100
+            print('Cluster max iterations not specified. Defaulting to 100')
+
+        if '_markov__lag' not in self.__dict__:
+            self._markov__lag = 1
+            print('Markov lag time not specified. Defaulting to 1')
+
+        if '_pcca_n' not in self.__dict__:
+            self._pcca_n = 5
+            print('Number of metastable states not specified. Defaulting to 5. Run set_pcca() to set the number of metastable states')
+        
+        self.model_dir.mkdir(exist_ok=True, parents=True)
+        self._save_model_params()
+        self.__str__()
+        
+
+    def __str__(self) -> str:
+        return f'MSMStudy: {self.study_name}\nWorking directory: {self.wk_dir}\nParameters: {self.get_paramters()}'
+
+
+    def get_paramters(self):
+        return ', '.join(f"{k}={v}" for k, v in self.__dict__.items() if k.strip('_') in self._allowed_paramters)
+    
+
+    def set_pcca(self, n):
+        if not isinstance(n, int):
+            raise ValueError('Number of metastable states must be an integer')
+        if hasattr(self, '_pcca_n') and hasattr(self, 'pcca_mod'):
+            print('Overwriting previous PCCA model')
+            self._pcca_n = n
+            self._pcca()
+        else:
+            self._pcca_n = n
+        self._save_model_params()
+
+
+    def _tica(self):
+        self.tica_mod = tica(self.ftrajs, lag=self._tica__lag, stride=self._tica__stride, 
+                             dim=self._tica__dim, kinetic_map=True)
+        self.ttrajs = self.tica_mod.get_output()
+        self.ttraj_cat = np.concatenate(self.ttrajs)
+
+
+    def _kmeans(self):
+        if not self.ttrajs:
+            raise ValueError('TICA trajectories not found. Run TICA first')
+        self.kmeans_mod = cluster_kmeans(self.ttrajs, k=self._cluster__k, max_iter=self._cluster__maxiter, 
+                                         stride=self._cluster__stride, fixed_seed=self._seed)
+        self.dtrajs = self.kmeans_mod.dtrajs
+        self.dtraj_cat = np.concatenate(self.dtrajs)
+        self.kmeans_centers = self.kmeans_mod.clustercenters
+
+
+    def _trans_count(self):
+        if not self.dtrajs:
+            raise ValueError('Discrete trajectories not found. Run kmeans clustering first')
+        self.count_mod = TransitionCountEstimator(lagtime=self._markov__lag, count_mode='sliding').fit_fetch(self.dtrajs)
+        self.connected_states = estimation.largest_connected_set(self.count_mod.count_matrix)
+        self.disconnected_states = np.setdiff1d(np.arange(self._cluster__k), self.connected_states)
+        if self.disconnected_states.shape[0] > 0:
+            print(f'Found disconnected states: {self.disconnected_states}')
+
+
+    def _ML_MSM(self):
+        if not self.count_mod:
+            raise ValueError('Transition count not found. Run transition count estimation first')
+        self.msm_mod = MaximumLikelihoodMSM(reversible=True).fit_fetch(self.count_mod)
+        self.traj_weights = self.msm_mod.compute_trajectory_weights(self.dtrajs)
+
+
+    def _bayesian_MSM(self):
+        # Implement Bayesian MSM in the future
+        '''
+        if not self.count_mod:
+            raise ValueError('Transition count not found. Run transition count estimation first')
+        self.msm_mod = BayesianMSM(reversible=True).fit_fetch(self.count_mod)
+        '''
+
+    def _pcca(self):
+        if not self.msm_mod:
+            raise ValueError('MSM not found. Run MSM estimation first')
+        if not hasattr(self, '_pcca_n'):
+            raise ValueError('Number of metastable states not set. Run set_pcca() to set the number of metastable states')
+        self.pcca_mod = self.msm_mod.pcca(n_metastable_sets=self._pcca_n)
+        self.pcca_assignment = self.pcca_mod.assignments
+        self.micro_to_macro = {self.connected_states[i]: self.pcca_assignment[i] for i in range(len(self.connected_states))}
+
+
+    def estimate_MSM(self):
+        self._tica()
+        self._kmeans()
+        self._trans_count()
+        self._ML_MSM()
+        self._pcca()
+        print('MSM estimation complete')
+
+
+    def transform(self, data_ftraj):
+        if not self.tica_mod:
+            raise ValueError('TICA model not found. Run estimate_MSM or restore_models first.')
+        if not self.kmeans_mod:
+            raise ValueError('Kmeans model not found. Run estimate_MSM or restore_models first.')
+        if not self.msm_mod:
+            raise ValueError('MSM model not found. Run estimate_MSM or restore_models first.')
+        if not self.pcca_mod:
+            raise ValueError('PCCA model not found. Run estimate_MSM or restore_models first.')
+        
+        ttraj = self.tica_mod.transform(data_ftraj)
+        dtraj = self.kmeans_mod.transform(ttraj)[0].flatten()
+        connected_d = np.array([idx for idx in dtraj if idx in self.connected_states]).flatten()
+        disconnected_d = np.array([idx for idx in dtraj if idx in self.disconnected_states]).flatten()
+        pcca_assignment = np.array([self.micro_to_macro[d] for d in connected_d])
+
+        return ttraj, connected_d, disconnected_d, pcca_assignment
+
+
+    def save_models(self, overwrite=True):
+        self._save_model_params()
+
+        if self.tica_mod:
+            self.tica_mod.save(str(self.model_dir / 'tica_model'), overwrite=overwrite)
+        if self.kmeans_mod:
+            self.kmeans_mod.save(str(self.model_dir / 'kmeans_model'), overwrite=overwrite)
+        if self.count_mod:
+            if overwrite:
+                np.save(self.model_dir/f'count_matrix.npy', self.count_mod.count_matrix)
+            else:
+                raise ValueError('Overwriting count matrix not allowed. Set overwrite=True to overwrite')
+        if self.msm_mod:
+            if overwrite:
+                np.save(self.model_dir/f'transition_matrix.npy', self.msm_mod.transition_matrix)
+            else:
+                raise ValueError('Overwriting transition matrix not allowed. Set overwrite=True to overwrite')
+    
+
+    def _save_model_params(self):
+        with open(self.model_dir / 'study_params.txt', 'w') as f:
+            for key, val in self.__dict__.items():
+                if key.strip('_') in self._allowed_paramters:
+                    f.write(f'{key}:{val}\n')
+        
+
+    def restore_models(self, model_dir=None):
+        if model_dir is None:
+            model_dir = self.wk_dir / 'models'
+
+        if isinstance(model_dir, str):
+            model_dir = Path(model_dir)
+
+        if not model_dir.is_dir():
+            raise ValueError('Models directory not found. Check the working directory')
+        
+        with open(model_dir / 'study_params.txt', 'r') as f:
+            for line in f:
+                key, val = line.split(':')
+                if key.strip('_') in self._allowed_paramters:
+                    self.__dict__[key] = int(val.strip('\n'))
+        
+        if (model_dir / 'tica_model').exists():
+            print('Restoring TICA model')
+            tica_mod = pm.coordinates.tica()
+            self.tica_mod = tica_mod.load(model_dir/'tica_model')
+            self.ttrajs = self.tica_mod.transform(self.ftrajs)
+            self.ttraj_cat = np.concatenate(self.ttrajs, axis=0)
+        else:
+            raise ValueError('TICA model not found. Check the working directory')
+        
+        if (model_dir / 'kmeans_model').exists():
+            print('Restoring Kmeans model')
+            kmeans_mod = pm.coordinates.cluster_kmeans()
+            self.kmeans_mod = kmeans_mod.load(model_dir/'kmeans_model')
+            self.dtrajs = [dtraj.flatten() for dtraj in self.kmeans_mod.transform(self.ttrajs)]
+            self.dtraj_cat = np.concatenate(self.dtrajs, axis=0)
+            self.kmeans_centers = self.kmeans_mod.clustercenters
+        else:
+            raise ValueError('Kmeans model not found. Check the working directory')
+        
+        self._trans_count()
+        self._ML_MSM()
+        self._pcca()
+        print('Models restore complete.')
+
+
---
+++ funcs_plotting.py
@@ -0,0 +1,332 @@
+# Functions for plotting MSM results
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+import networkx as nx
+from deeptime.plots import plot_implied_timescales, plot_energy2d, plot_contour2d_from_xyz
+from deeptime.util import energy2d
+
+import numpy as np
+
+
+def plot_ev(ev, c_centers, traj_all, traj_weights, title, savedir, dim_1=0, dim_2=1, dim_3=2, \
+            ct_cmap='nipy_spectral', ct_a=0.6, ev_cmap='coolwarm', ev_a=0.8, ev_s=20, ev_marker='.', \
+            ex=True, ex_s=100):
+
+    vmin, vmax = min(ev), max(ev)
+    divnorm = mpl.colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+    min_id, max_id = ev.argmin(), ev.argmax()
+
+    fig = plt.figure(figsize=(8, 8))
+    ax1 = fig.add_axes([0, 0, 0.5, 0.5])
+    ax2 = fig.add_axes([0.52, 0, 0.5, 0.5])
+    fe_cax = fig.add_axes([ax1.get_position().x0-0.08, ax1.get_position().y0, 0.03, ax1.get_position().height])
+    ev1_cax = fig.add_axes([ax1.get_position().x0, ax1.get_position().y1+0.02, ax1.get_position().width, 0.03])
+    ev2_cax = fig.add_axes([ax2.get_position().x0, ax2.get_position().y1+0.02, ax2.get_position().width, 0.03])
+
+    ax1, contour1, cbar1 = plot_energy2d(energy2d(traj_all[:,dim_1], traj_all[:,dim_2], weights=traj_weights), ax=ax1, cbar_ax=fe_cax, contourf_kws=dict(cmap=ct_cmap, alpha=ct_a))
+    ax2, contour1, cbar2 = plot_energy2d(energy2d(traj_all[:,dim_3], traj_all[:,dim_2], weights=traj_weights), ax=ax2, cbar=False, contourf_kws=dict(cmap=ct_cmap, alpha=ct_a))
+
+    ax1_eg2 = ax1.scatter(c_centers[:,dim_1], c_centers[:,dim_2], s=ev_s, c=ev, marker=ev_marker, alpha=ev_a, cmap=ev_cmap, norm=divnorm)
+    fig.colorbar(ax1_eg2, cax=ev1_cax, format='%.1f', orientation='horizontal')
+
+    ax2_eg2 = ax2.scatter(c_centers[:,dim_3], c_centers[:,dim_2], s=ev_s, c=ev, marker=ev_marker, alpha=ev_a, cmap=ev_cmap, norm=divnorm)
+    fig.colorbar(ax2_eg2, cax=ev2_cax, format='%.1f', orientation='horizontal')
+    
+    if ex:
+        ax1.scatter(c_centers[min_id,dim_1], c_centers[min_id,dim_2], s=ex_s, c='k', marker='X', label='min')
+        ax1.scatter(c_centers[max_id,dim_1], c_centers[max_id,dim_2], s=ex_s, c='k', marker='v', label='max')
+        ax2.scatter(c_centers[min_id,dim_3], c_centers[min_id,dim_2], s=ex_s, c='k', marker='X', label='min')
+        ax2.scatter(c_centers[max_id,dim_3], c_centers[max_id,dim_2], s=ex_s, c='k', marker='v', label='max')
+        ax1.legend()
+        ax2.legend()
+
+    ev1_cax.tick_params(axis='x', bottom=False, top=True, labeltop=True, labelbottom=False)
+    ev2_cax.tick_params(axis='x', bottom=False, top=True, labeltop=True, labelbottom=False)
+    ax2.tick_params(axis='y', left=False, right=True, labelleft=False, labelright=True)
+    fe_cax.tick_params(axis='y', left=True, right=False, labelleft=True, labelright=False)
+
+    ax1.set_xlabel(f'tIC {dim_1+1}', fontsize=14)
+    ax2.set_xlabel(f'tIC {dim_3+1}', fontsize=14)
+    ax2.set_ylabel(f'tIC {dim_2+1}', fontsize=14)
+    ax2.yaxis.set_label_position("right")
+    ev1_cax.set_xlabel(title, fontsize=14)
+    ev2_cax.set_xlabel(title, fontsize=14)
+    ev1_cax.xaxis.set_label_position('top')
+    ev2_cax.xaxis.set_label_position('top')
+    fe_cax.set_ylabel('Free energy (kT)', fontsize=14)
+    fe_cax.yaxis.set_label_position("left")
+
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None
+
+
+def plot_fe(traj_all, traj_weights, savedir, fes_cmap='nipy_spectral', 
+            dim_1 = 0, dim_2 = 1, \
+            c_centers=None, d_centers=None, 
+            c_centers_s=10, c_centers_marker='.', c_centers_a=0.8, c_centers_c='black',
+            d_centers_s=10, d_centers_marker='X', d_centers_a=0.8, d_centers_c='black', d_edgecolor='white', d_linewidth=1,
+            state_assignment=None, state_population=None, pcca_cmap='gist_rainbow', edgecolor='black', linewidth=1,
+            legend_marker_sizes=100,
+            title=''):
+    
+    fig, ax = plt.subplots(figsize=(7, 6))
+    ax, contour, cbar = plot_energy2d(energy2d(traj_all[:, dim_1], traj_all[:, dim_2], weights=traj_weights), ax=ax, contourf_kws=dict(cmap=fes_cmap))
+    
+    if state_assignment is not None:
+        n_states = len(np.unique(state_assignment))
+        colours = [plt.cm.get_cmap(pcca_cmap)(i/(n_states-1)) for i in range(n_states)]
+        for i in range(n_states):
+            ax.scatter(c_centers[state_assignment == i, dim_1], c_centers[state_assignment == i, dim_2], 
+                       s=c_centers_s, c=colours[i], marker=c_centers_marker, alpha=c_centers_a, 
+                       edgecolor=edgecolor, linewidth=linewidth,
+                       label=f'macrostate {i+1} ({state_population[i]*100:.1f}%)')
+        legend = plt.legend(markerscale=1, loc='best', fontsize=10)
+        
+    if (state_assignment is  None) and (c_centers is not None): 
+        ax.scatter(c_centers[:,dim_1], c_centers[:,dim_2], s=c_centers_s, c=c_centers_c, marker=c_centers_marker, alpha=c_centers_a)
+    
+    if d_centers is not None:
+        ax.scatter(d_centers[:,dim_1], d_centers[:,dim_2], s=d_centers_s, c=d_centers_c, marker=d_centers_marker, alpha=d_centers_a, 
+                   edgecolor=d_edgecolor, linewidth=d_linewidth, label='disconnected states')
+        legend = plt.legend(markerscale=1, loc='best', fontsize=10)
+
+    ax.set_xlabel(f'tIC {dim_1+1}', fontsize=14)
+    ax.set_ylabel(f'tIC {dim_2+1}', fontsize=14)
+    cbar.ax.set_ylabel('Free energy (kT)', fontsize=14)
+    if title is not None: ax.set_title(title, fontsize=16)
+
+    try:
+        if type(legend_marker_sizes) is int: legend_marker_sizes = [legend_marker_sizes] * len(legend.legend_handles)
+        for i, handle in enumerate(legend.legend_handles):
+            handle.set_sizes([legend_marker_sizes[i]])
+    except:
+        pass
+
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None
+
+
+def plot_pcca_graph(traj_all, traj_weights,  
+                    c_centers, matrix, pcca_assignment, stat_dist, 
+                    fes_cmap='nipy_spectral', dim_1 = 0, dim_2 = 1, 
+                    c_centers_s=50, c_centers_marker='.', c_centers_a=0.5, 
+                    pcca_cmap='gist_rainbow', c_edgecolor='black', linewidth=1, 
+                    g_alpha=0.8, connectionstyle='Angle3', 
+                    savedir=None):
+    
+    n_states = len(np.unique(pcca_assignment))
+    colours = [cm.get_cmap(pcca_cmap)(i/(n_states-1)) for i in range(n_states)]
+    
+    # Compute the centeroids of pcca macrostates 
+    # Using the microstate centers weighted by the stationary probability
+    macrostate_centroid = []
+    for i in range(n_states):
+        macrostate_cluster_centers = c_centers[pcca_assignment == i, :]
+        macrostate_cluster_weights = stat_dist[pcca_assignment == i]
+        weighted_centroid = np.average(macrostate_cluster_centers, axis=0, weights=macrostate_cluster_weights)
+        macrostate_centroid.append(weighted_centroid)
+    pos = {i:(macrostate_centroid[i][dim_1], macrostate_centroid[i][dim_2]) for i in range(n_states)}
+
+    # Node sizes are scaled by the stationary probability of the macrostate
+    cg_stationary_dist = [sum(stat_dist[pcca_assignment == i]) for i in range(n_states)]
+    node_sizes = cg_stationary_dist/min(cg_stationary_dist)*100
+    node_ln_widths = np.cbrt(node_sizes/min(node_sizes))
+    node_ft_sizes = np.cbrt(node_sizes/min(node_sizes))*8
+
+
+    fig, ax = plt.subplots(figsize=(11, 10))
+    # Plot the FES as a background
+    ax, contour, cbar = plot_energy2d(energy2d(traj_all[:, dim_1], traj_all[:, dim_2], weights=traj_weights), ax=ax, contourf_kws=dict(cmap=fes_cmap))
+    # Plot the microstate centers coloured by pcca assignment
+    for i in range(n_states):
+        ax.scatter(c_centers[pcca_assignment == i, dim_1], c_centers[pcca_assignment == i, dim_2], 
+                    s=c_centers_s, c=colours[i], marker=c_centers_marker, alpha=c_centers_a, 
+                    edgecolor=c_edgecolor, linewidth=linewidth)
+
+    ax.set_xlabel(f'tIC {dim_1+1}', fontsize=14)
+    ax.set_ylabel(f'tIC {dim_2+1}', fontsize=14)
+    cbar.ax.set_ylabel('Free energy (kT)', fontsize=14)
+
+    G = nx.DiGraph()
+    # Add nodes and edges to the graph
+    for i in range(n_states):
+        G.add_node(i, label=f'{i+1}')
+        for j in range(n_states):
+            if (i != j) and (matrix[i, j] < 3000000):
+                G.add_edge(i, j, weight=np.log2(max(matrix.flatten()) / matrix[i, j]))
+    edge_widths = [edge[2]['weight'] for edge in G.edges(data=True)]
+
+    nx.draw(G, pos, 
+            node_size=node_sizes[[node[0] for node in G.nodes(data=True)]],
+            node_color=[colours[node[0]] for node in list(G.nodes(data=True))], 
+            edgecolors='black', linewidths=node_ln_widths[[node[0] for node in G.nodes(data=True)]], 
+            alpha=g_alpha, 
+            edge_color = [colours[edge[0]] for edge in G.edges(data=True)], 
+            width=edge_widths, arrows=True, connectionstyle=connectionstyle,
+            ax=ax)
+    # Add labels to the nodes
+    node_labels = nx.get_node_attributes(G, 'label')
+    for node, (x, y) in pos.items():
+        label = node_labels[node]
+        ax.text(x, y, label, fontsize=node_ft_sizes[int(node)], ha='center', va='center')
+
+    plt.tight_layout(pad=2.0)
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None
+
+
+def plot_ts(timescales, n_ts, markov_lag, savedir, scaling=0.00005, unit="$\mathrm{\mu s}$"):
+    
+    fig, ax = plt.subplots(figsize=(8, 4))
+    x = np.arange(n_ts)+2
+    y = timescales[:n_ts]*scaling
+    markov_lag_scaled = markov_lag * scaling
+
+    ax.plot(x, y, marker='o', markersize=10)
+    ax.fill_between([0, n_ts+2], y1=markov_lag_scaled, y2=0, color='grey', alpha=0.8)
+
+    ax.set_yscale('log')
+    ax.grid(visible=True, axis='y')
+
+    ax.set_xlim([1, n_ts+2])
+    ax.set_ylim([min(y)/3, max(y)*3])
+
+    ax.set_xticks(np.arange(2, n_ts+2, 1))
+    ax.set_ylabel(rf"Timescale ({unit})", fontsize=14)
+    ax.set_xlabel(r"Timescale Index", fontsize=14)
+    ax.tick_params(bottom=True, top=False, left=True, right=False)
+
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None
+
+
+
+def plot_pcca(state_assignment, c_centers, savedir, dim_1=0, dim_2=1, \
+              c_centers_s=6, cmap='gist_rainbow'):
+    
+    n_states = len(np.unique(state_assignment))
+    cmap = mpl.colormaps[f'{cmap}'].resampled(n_states)
+    norm = colors.BoundaryNorm(list(range(0, n_states+1)), n_states, clip=True)
+
+    fig, ax=plt.subplots(1,1,figsize=(6, 6))
+    s = ax.scatter(c_centers[:,dim_1], c_centers[:,dim_2], 
+                   c=state_assignment, s=c_centers_s, cmap=cmap, norm=norm)
+    ax.set_xlabel(f'tIC {dim_1+1}', fontsize=14)
+    ax.set_ylabel(f'tIC {dim_2+1}', fontsize=14)
+    ax.tick_params(axis='both', which='major', labelsize=12)
+
+    cbar_ax = fig.add_axes([ax.get_position().x1+0.02, ax.get_position().y0, 0.03, ax.get_position().height])
+    cbar = plt.colorbar(s, cax=cbar_ax, label='Macrostate')
+    cbar.set_ticks(np.arange(n_states) + 0.5)
+    cbar.set_ticklabels(np.arange(n_states) + 1)
+    cbar_ax.tick_params(labelsize=12)
+    cbar_ax.set_ylabel('Macrostate', fontsize=14)
+
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None
+
+
+
+def plot_mfpt_matrix(mfpt, savedir, scaling=0.00005, unit="$\mathrm{\mu s}$", text_f =".2e"):
+    n_states = mfpt.shape[0]
+    mfpt_scaled = mfpt*scaling # How many microseconds per step?
+    norm = mpl.colors.Normalize(vmin=np.min(mfpt_scaled), vmax=np.max(mfpt_scaled))
+
+    fig,ax = plt.subplots(1,figsize=(10,10))
+    s = ax.imshow(mfpt_scaled, cmap='turbo', norm=norm) 
+    for i in range(n_states):
+        for j in range(n_states):        
+                ax.text(j, i, "{value:{format}}".format(value=mfpt_scaled[i,j], format=text_f), va='center', ha='center', color='white', size=10, weight="bold")
+
+    cbar_ax = fig.add_axes([ax.get_position().x1+0.02, ax.get_position().y0, 0.03, ax.get_position().height])
+    cbar = plt.colorbar(s, cax=cbar_ax)
+
+    ax.set_xlabel('State$_j$', fontsize=18)
+    ax.set_ylabel('State$_i$', fontsize=18)
+    ax.set_xticks(np.arange(n_states))
+    ax.set_yticks(np.arange(n_states))
+    ax.set_xticklabels(np.arange(n_states)+1, fontsize=14)
+    ax.set_yticklabels(np.arange(n_states)+1, fontsize=14)
+    ax.tick_params(labelsize=14)
+    ax.set_title('MFPT Matrix', fontsize=25)
+
+    cbar.ax.tick_params(labelsize=16)
+    cbar.ax.set_ylabel(rf"State$_i$ --> State$_j$ MFPT ({unit})", fontsize=16)
+
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None
+
+
+def plot_dihed_pie(spatial_counts, dihed_counts, radius_size=0.5, 
+                   figsize=(6,6), title='', fontsize=12, savedir=None):
+    
+    dihed_cluster_labels = ['noise', 'BLAminus', 'BLAplus', 'ABAminus', 'BLBminus', 'BLBplus', 'BLBtrans', 'noise', 'BABtrans', 'noise', 'BBAminus']
+    spatial_cluster_labels = ['DFG-in', 'DFG-inter', 'DFG-out']
+    
+    outer_colors = [
+    (128/255, 128/255, 128/255),  # Gray
+    (235/255, 95/255, 70/255),    # Light Red
+    (240/255, 146/255, 58/255),   # Flamebright
+    (255/255, 214/255, 92/255),   # Light yellow
+    (255/255, 188/255, 214/255),  # Light pink
+    (210/255, 180/255, 140/255),  # Tan
+    (196/255, 79/255, 108/255),   # Strawberry
+    (128/255, 128/255, 128/255),  # Gray
+    (25/255, 189/255, 85/255),    # Light Green
+    (128/255, 128/255, 128/255),  # Gray
+    (136/255, 75/255, 204/255)]   # Light Purple
+    inner_colors = [
+     (173/255, 35/255, 10/255),  # Red
+     (28/225, 128/255, 65/255),  # Green
+     (80/255, 29/255, 138/255)]  # Purple
+
+    filtered_spatial_cluster_labels = [label if count/sum(spatial_counts) >= 0.05 else '' for label, count in zip(spatial_cluster_labels, spatial_counts)]
+    filtered_dihed_cluster_labels = [label if count/sum(sum(dihed_counts,[])) >= 0.05 else '' for label, count in zip(dihed_cluster_labels, sum(dihed_counts,[]))]
+
+    fig, ax = plt.subplots(figsize=figsize)
+ 
+    wedges_i, texts_i = ax.pie(spatial_counts, radius=1-radius_size, colors=inner_colors, 
+           # labels=filtered_spatial_cluster_labels, textprops={'fontsize': fontsize, 'fontweight': 'bold'}, labeldistance=0.5, # Don't show the spatial labels
+           wedgeprops=dict(width=radius_size, edgecolor='w'))
+
+    wedges_o, texts_o = ax.pie(sum(dihed_counts,[]), radius=1, colors=outer_colors,
+           #labels=filtered_dihed_cluster_labels, textprops={'fontsize': fontsize, 'fontweight': 'bold'}, labeldistance=0.7,
+           wedgeprops=dict(width=radius_size, edgecolor='w'))
+
+    ax.set(aspect="equal")
+    ax.text(0, 0, title, ha='center', va='center', fontsize=16, fontweight='bold')
+
+    
+    ax.legend(wedges_i, spatial_cluster_labels,
+              loc="center left",
+              bbox_to_anchor=(1, 0, 0.5, 1),
+              fontsize=18,
+              markerscale=2)
+    
+    
+    if savedir is not None:
+        plt.savefig(savedir, transparent=True, bbox_inches='tight', dpi=300)
+    plt.show()
+
+    return None 
